52fea460578c25e49920ca0b3cf42bef31c89604
Stels67 <123122149+Stels67@users.noreply.github.com>
2023-09-27 02:10:21 +0300

    Добавьте файлы проекта.

---
+++ parsing xml files/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿namespace parsing_xml_files
+{
+    public class PersonModel
+    {   
+        public string FirstName { get; set; }
+
+        public string Surname{ get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string PassportNumber { get; set; }
+
+    }
+}
---
+++ parsing xml files/Program.cs
@@ -0,0 +1,24 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace parsing_xml_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var file = new FileInfo(fileName: @"C:\Users\gjghj\OneDrive\Рабочий стол\Exel Files\Test.xlsx");
+        }
+
+        static List<PersonModel> GetSetupData()
+        {
+            List<PersonModel> output = new()
+            {
+                new() { }
+            }
+        }
+    }
+}
